9101fef17b5a0a707c5a08b528d290b519ba1085
gabrielpit <gabrielpit2k19@gmail.com>
2024-08-14 21:37:25 -0300

    Implementa gerador basico de apostas

---
+++ GeradorDeApostas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem vindo ao gerador de apostas");
+
+        int[] numeros = new int[6];
+        Random random = new Random();
+        //Gera um numero aleatorio
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = random.Next(1, 99);
+        }
+        //Gera um numero aleatorio para cada espaco no array numeros
+
+        Console.WriteLine("Seus numeros da sorte sao:");
+
+        foreach (int numero in numeros)
+        //Percorre o array 1 por 1
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
